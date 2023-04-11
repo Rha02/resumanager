92104fa2fc992310a96c2864929369e3dce47f26
Rha02 <romuzus08@gmail.com>
2023-04-11 16:00:51 -0700

    Created User model

--- src/handlers/user_handlers.go
+++ src/handlers/user_handlers.go
@@ -1,7 +1,27 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/Rha02/resumanager/src/models"
+)
 
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+	reqBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	var user models.User
+
+	err = json.Unmarshal(reqBody, &user)
+	if err != nil {
+		http.Error(w, "Error unmarshalling request body", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("Hello " + user.Username))
 }
